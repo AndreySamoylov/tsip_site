9baf8dae333a7caf44fc5bc667649c77eea077f6
Андрей Самойлов <san@d-element.ru>
2025-05-09 21:40:47 +0500

    deploy bitrix

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Фильмы");
+?>
+
+Text ...
+
+<?php require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php"); ?>
