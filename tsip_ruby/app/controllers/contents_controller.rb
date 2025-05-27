d81504deaacf47384230c6a599d6c799ef1dd250
Alexander <alexander@Alexanders-MacBook-Air.local>
2025-05-27 18:16:37 +0500

    move token

--- tsip_ruby/app/controllers/contents_controller.rb
+++ tsip_ruby/app/controllers/contents_controller.rb
@@ -11,7 +11,7 @@ class ContentsController < ApplicationController
     uri = URI("https://kinopoiskapiunofficial.tech/api/v2.2/films/")
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
-    request = Net::HTTP::Get.new(uri.path + params[:id], {'Content-Type' => 'application/json', "X-API-KEY"=> "8c8e1a50-6322-4135-8875-5d40a5420d86"})
+    request = Net::HTTP::Get.new(uri.path + params[:id], {'Content-Type' => 'application/json', "X-API-KEY"=> ENV['KINOPOISK_TOKEN']})
     response = http.request(request)
     @content = JSON.parse(response.body)
   end
